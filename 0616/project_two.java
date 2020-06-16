4e6c6b1cc4d66a5a22a612878738689654fcd025
Kohansol <64995062+khs-st@users.noreply.github.com>
2020-06-16 16:43:28 +0900

    자바

---
+++ 0616/project_two.java
@@ -0,0 +1,21 @@
+class project_two {
+    public static void main(String[] args) {
+        System.out.println("hihihi");
+        System.out.println("dddd");
+
+        System.out.println(7);
+        System.out.println(73.15);
+
+        System.out.println(7);
+
+        System.out.println(7);
+
+        System.out.println(7);
+
+        
+        System.out.println(7);
+
+        System.out.println(7);
+
+    }
+}
